6f7919f52144c4e6c741630b28cfcce7d74630ce
Dan Hausner <dn.hausner@gmail.com>
2019-06-21 16:41:23 +0100

    Refactor, use UART and Exception instead of Serial and SerialException

--- lib/plantower.py
+++ lib/plantower.py
@@ -7,15 +7,15 @@
 
 import logging
 import time
-from machine import Timer
-from serial import Serial, SerialException
+from machine import Timer, UART
 
 timestamp_template = "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}"  # yyyy-mm-dd hh-mm-ss
 
-DEFAULT_SERIAL_PORT = ('P10', 'P11')  # Serial port to use if no other specified, (TX, RX)
+DEFAULT_SERIAL_PINS = ('P10', 'P11')  # Serial pins to use if no other specified, (TX, RX)
 DEFAULT_BAUD_RATE = 9600  # Serial baud rate to use if no other specified
 DEFAULT_SERIAL_TIMEOUT = 2  # Serial timeout to use if not specified
 DEFAULT_READ_TIMEOUT = 1  # How long to sit looking for the correct character sequence.
+DEFAULT_ID = 1  # UART bus id
 
 DEFAULT_LOGGING_LEVEL = logging.WARN
 
@@ -68,10 +68,14 @@ class Plantower(object):
     """
 
     def __init__(
-            self, port=DEFAULT_SERIAL_PORT, baud=DEFAULT_BAUD_RATE,
+            self,
+            pins=DEFAULT_SERIAL_PINS,
+            baud=DEFAULT_BAUD_RATE,
             serial_timeout=DEFAULT_SERIAL_TIMEOUT,
             read_timeout=DEFAULT_READ_TIMEOUT,
-            log_level=DEFAULT_LOGGING_LEVEL):
+            log_level=DEFAULT_LOGGING_LEVEL,
+            id=DEFAULT_ID
+    ):
         """
             Setup the interface for the sensor
         """
@@ -79,21 +83,25 @@ class Plantower(object):
         logging.basicConfig(
             format='%(asctime)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s')
         self.logger.setLevel(log_level)
-        self.port = port
-        self.logger.info("Serial port: %s", self.port)
+        self.pins = pins
+        self.logger.info("Serial pins: %s", self.pins)
         self.baud = baud
         self.logger.info("Baud rate: %s", self.baud)
         self.serial_timeout = serial_timeout
         self.logger.info("Serial Timeout: %s", self.serial_timeout)
         self.read_timeout = read_timeout
         self.logger.info("Read Timeout: %s", self.read_timeout)
+        self.id = id
         self.chrono = Timer.Chrono()
         try:
-            self.serial = Serial(
-                port=self.port, baudrate=self.baud,
-                timeout=self.serial_timeout)
+            self.serial = UART(
+                self.id,
+                baudrate=self.baud,
+                pins=self.pins,
+                timeout_chars=self.serial_timeout
+            )
             self.logger.debug("Port Opened Successfully")
-        except SerialException as exp:
+        except Exception as exp:
             self.logger.error(str(exp))
             raise PlantowerException(str(exp))
 
